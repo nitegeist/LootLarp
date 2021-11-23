bcda4bf94dbe6e4fcb85f26d0ddb116eb38ac40f
Nitegeist <delroybicknell@gmail.com>
2021-11-23 00:41:51 -0500

    Update smart contract functionality

--- contracts/Larp.sol
+++ contracts/Larp.sol
@@ -1,166 +1,75 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
 
 /**
- * @title ERC721 token for LootLarp
+ * @title ERC721 Smart Contract for LootLARP
  *
  * @author Nitegeist
  */
 contract Larp is
+    IERC721Metadata,
     ERC721URIStorage,
-    Pausable,
-    AccessControl,
-    ERC721Burnable,
+    ERC721PresetMinterPauserAutoId,
     ReentrancyGuard
 {
     using Strings for uint256;
     using Counters for Counters.Counter;
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant PREFERRED_MINTER_ROLE =
         keccak256("PREFERRED_MINTER_ROLE");
-    Counters.Counter private _totalPublicMinted;
-    Counters.Counter private _totalPrivateMinted;
-
-    uint256 listingPrice = 250000000000000000; // 0.25 ETH
+    Counters.Counter private _totalMinted;
+    uint256 listingPrice = 25 * 10e15; // 0.25 ETH
 
     // Address of interface identifier for royalty standard
     bytes4 private constant INTERFACE_ID_ERC2981 = 0x2a55205a;
 
     // Token ID constants
-    uint256 private constant TOTAL_ADDITIONAL_TOKENS = 100;
-    uint256 private constant TOTAL_PUBLIC_TOKENS = 400;
+    uint256 private constant TOTAL_CLAIMABLE_TOKENS = 500;
     uint256 private constant TOTAL_LEGENDARY_TOKENS = 8;
     uint256 private constant TOTAL_SUPPLY = 508;
+
+    // baseUri
+    string public constant BASE_URI = "ipfs://";
+
+    // private claim
+    uint256 startTime;
+    uint256 endTime;
 
     // Status of public claim
     bool public publicClaim;
 
-    // Array of token ids for public mint
-    uint256[] publicTokenIds;
+    // Status of public claim
+    bool public privateRedeem;
 
-    // Status of private claim
-    bool public privateClaim;
-
-    // Array of token ids for private mint
-    uint256[] privateTokenIds;
-
-    constructor() ERC721("Larp", "LARP") {
-        // Grant the deployer of the contract default admin role
-        // This role can grant and revoke other roles
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        publicClaim = false;
-        privateClaim = false;
+    constructor(uint256 _startTime)
+        ERC721PresetMinterPauserAutoId("Larp", "LARP", BASE_URI)
+    {
+        startTime = _startTime;
+        endTime = _startTime + 1 days;
     }
 
-    function pause() public onlyRole(PAUSER_ROLE) {
-        _pause();
-    }
-
-    function unpause() public onlyRole(PAUSER_ROLE) {
-        _unpause();
-    }
-
-    // Toggle public claim
-    function togglePublicClaim() external onlyRole(DEFAULT_ADMIN_ROLE) {
-        publicClaim = !publicClaim;
-    }
-
-    // Toggle private claim
-    function togglePrivateClaim() external onlyRole(DEFAULT_ADMIN_ROLE) {
-        privateClaim = !privateClaim;
-    }
-
-    // Grants minter role
-    function grantMinterRole(address _account)
+    // Returns baseURI
+    function _baseURI()
         internal
-        onlyRole(DEFAULT_ADMIN_ROLE)
+        view
+        virtual
+        override(ERC721, ERC721PresetMinterPauserAutoId)
+        returns (string memory)
     {
-        // Checks if address already has this role
-        require(
-            !hasRole(MINTER_ROLE, _account),
-            "This address has already been assigned this role"
-        );
-        grantRole(MINTER_ROLE, _account);
+        return BASE_URI;
     }
 
-    // Revokes minter role
-    function revokeMinterRole(address _account)
-        internal
-        onlyRole(DEFAULT_ADMIN_ROLE)
-    {
-        // Checks if address doesn't have this role
+    // Batch grants preferred minter role
+    function batchGrantPreferredMinterRole(address[] memory _accounts) public {
         require(
-            hasRole(MINTER_ROLE, _account),
-            "This address does not have this role"
+            hasRole(DEFAULT_ADMIN_ROLE, _msgSender()),
+            "Must have admin role grant preferred minter role"
         );
-        revokeRole(MINTER_ROLE, _account);
-    }
-
-    // Grants pauser role
-    function grantPauserRole(address _account)
-        internal
-        onlyRole(DEFAULT_ADMIN_ROLE)
-    {
-        // Checks if address already has this role
-        require(
-            !hasRole(PAUSER_ROLE, _account),
-            "This address has already been assigned this role"
-        );
-        grantRole(PAUSER_ROLE, _account);
-    }
-
-    // Revokes pauser role
-    function revokePauserRole(address _account)
-        internal
-        onlyRole(DEFAULT_ADMIN_ROLE)
-    {
-        // Checks if address doesn't have this role
-        require(
-            hasRole(PAUSER_ROLE, _account),
-            "This address does not have this role"
-        );
-        revokeRole(PAUSER_ROLE, _account);
-    }
-
-    // Grants preferred minter role
-    function grantPreferredMinterRole(address _account)
-        internal
-        onlyRole(DEFAULT_ADMIN_ROLE)
-    {
-        // Checks if address already has this role
-        require(
-            !hasRole(PREFERRED_MINTER_ROLE, _account),
-            "This address has already been assigned this role"
-        );
-        grantRole(PREFERRED_MINTER_ROLE, _account);
-    }
-
-    // Revokes preferred minter role
-    function revokePreferredMinterRole(address _account)
-        internal
-        onlyRole(DEFAULT_ADMIN_ROLE)
-    {
-        // Checks if address doesn't have this role
-        require(
-            hasRole(PREFERRED_MINTER_ROLE, _account),
-            "This address does not have this role"
-        );
-        revokeRole(PREFERRED_MINTER_ROLE, _account);
-    }
-
-    // Batch grants roles
-    function batchGrantPreferredMinterRole(address[] memory _accounts)
-        public
-        onlyRole(DEFAULT_ADMIN_ROLE)
-    {
         for (uint256 i = 0; i < _accounts.length; i++) {
             // Checks if addresses already have this role
             require(
@@ -171,11 +80,14 @@ contract Larp is
         }
     }
 
-    // Batch revokes roles
+    // Batch revokes preferred minter role
     function batchRevokePreferredMinterRole(address[] memory _accounts)
-        public
-        onlyRole(DEFAULT_ADMIN_ROLE)
+        external
     {
+        require(
+            hasRole(DEFAULT_ADMIN_ROLE, _msgSender()),
+            "Must have admin role revoke preferred minter role"
+        );
         for (uint256 i = 0; i < _accounts.length; i++) {
             // Checks if addresses don't have this role
             require(
@@ -186,98 +98,107 @@ contract Larp is
         }
     }
 
-    // Checks if an account has a role
-    function accountHasRole(address _account, string memory _role)
-        internal
-        view
-    {
-        _checkRole(keccak256(abi.encodePacked(_role)), _account);
+    // Toggle public claim
+    function togglePublicClaim() external {
+        require(
+            hasRole(DEFAULT_ADMIN_ROLE, _msgSender()),
+            "Must have admin role to toggle public claim"
+        );
+        require(block.timestamp > endTime, "Private claim has not ended");
+        publicClaim = !publicClaim;
+    }
+
+    // Toggle private redeem
+    function togglePrivateRedeem() external {
+        require(
+            hasRole(DEFAULT_ADMIN_ROLE, _msgSender()),
+            "Must have admin role to toggle private redeem"
+        );
+        require(block.timestamp > endTime, "Private claim has not ended");
+        require(!publicClaim, "Public claim has not ended");
+        privateRedeem = !privateRedeem;
     }
 
     // Gets listing price
-    function getListingPrice() public view returns (uint256) {
+    function getListingPrice() external view returns (uint256) {
         return listingPrice;
     }
 
+    // Get available supply
+    function getAvailableSupply() external view returns (uint256) {
+        return TOTAL_SUPPLY - _totalMinted.current();
+    }
+
     // Sets listing price in wei
-    function setListingPrice(uint256 _wei) public onlyRole(DEFAULT_ADMIN_ROLE) {
+    function setListingPrice(uint256 _wei) external {
+        require(
+            hasRole(DEFAULT_ADMIN_ROLE, _msgSender()),
+            "Must have admin role to set price"
+        );
         listingPrice = _wei;
     }
 
-    // Sets an array of public token ids
-    // Requires public ids not to exceed the max amount of public ids that can be generated
-    function setPublicTokenIds(uint256[] memory _tokenIds)
-        external
-        onlyRole(DEFAULT_ADMIN_ROLE)
-    {
-        require(
-            _tokenIds.length <= TOTAL_PUBLIC_TOKENS,
-            "Token amount exceeds public token limit"
-        );
-        for (uint256 i = 0; i < _tokenIds.length; i++) {
-            publicTokenIds.push(_tokenIds[i]);
-        }
-    }
-
-    // Sets an array of private token ids
-    // Requires private ids not to exceed the max amount of private ids that can be generated
-    function setPrivateTokenIds(uint256[] memory _tokenIds)
-        external
-        onlyRole(DEFAULT_ADMIN_ROLE)
-    {
-        require(
-            _tokenIds.length <= TOTAL_LEGENDARY_TOKENS,
-            "Token amount exceeds private token limit"
-        );
-        for (uint256 i = 0; i < _tokenIds.length; i++) {
-            privateTokenIds.push(_tokenIds[i]);
-        }
-    }
-
     // Public mint function
-    function publicMint(string memory tokenUri)
-        public
-        payable
-        onlyRole(MINTER_ROLE)
-        nonReentrant
-    {
-        address owner = _msgSender();
+    function publicMint(string memory tokenUri) external payable nonReentrant {
         require(
-            hasRole(MINTER_ROLE, owner),
-            "This address is not a preferred minter"
+            msg.value == listingPrice,
+            "Public Mint: Incorrect payment amount"
         );
         require(
-            balanceOf(owner) <= 1,
+            balanceOf(_msgSender()) <= 2,
             "Only two tokens can be minted per address"
         );
-        require(publicClaim == true, "Public mint is not active");
-        uint256 totalMinted = _totalPublicMinted.current();
-        require(totalMinted < publicTokenIds.length, "Public mint has ended");
-        _mintToken(totalMinted, tokenUri, owner);
-        _totalPublicMinted.increment();
+        require(publicClaim, "Public mint is not active");
+        uint256 totalMinted = _totalMinted.current();
+        _mintToken(totalMinted, tokenUri, _msgSender());
+        _totalMinted.increment();
     }
 
     // Private mint function
-    function privateMint(string memory tokenUri)
-        public
+    function privateMint(string memory tokenUri) external payable nonReentrant {
+        require(
+            hasRole(PREFERRED_MINTER_ROLE, _msgSender()),
+            "Private Mint: Must have preferred minter role to mint"
+        );
+        require(
+            msg.value == listingPrice,
+            "Private Mint: Incorrect payment amount"
+        );
+        require(
+            balanceOf(_msgSender()) <= 2,
+            "Private Mint: Only two tokens can be minted per address"
+        );
+        require(
+            block.timestamp > startTime && block.timestamp < endTime,
+            "Private Mint: Private mint inactive"
+        );
+        uint256 totalMinted = _totalMinted.current();
+        _mintToken(totalMinted, tokenUri, _msgSender());
+        _totalMinted.increment();
+    }
+
+    // Door staff mint function
+    function privateEventRedeem(string memory tokenUri)
+        external
         payable
-        onlyRole(PREFERRED_MINTER_ROLE)
         nonReentrant
     {
-        address owner = _msgSender();
         require(
-            hasRole(PREFERRED_MINTER_ROLE, owner),
-            "This address is not a preferred minter"
+            hasRole(MINTER_ROLE, _msgSender()),
+            "Private Redeem: Must have minter role to mint"
         );
         require(
-            balanceOf(owner) <= 1,
-            "Only two tokens can be minted per address"
+            msg.value == listingPrice,
+            "Private Redeem: Incorrect payment amount"
         );
-        require(privateClaim == true, "Private mint is not active");
-        uint256 totalMinted = _totalPrivateMinted.current();
-        require(totalMinted < privateTokenIds.length, "Private mint has ended");
-        _mintToken(totalMinted, tokenUri, owner);
-        _totalPrivateMinted.increment();
+        require(
+            balanceOf(_msgSender()) <= 2,
+            "Private Redeem: Only two tokens can be minted per address"
+        );
+        require(privateRedeem, "Private Redeem: Private redeem is not active");
+        uint256 totalMinted = _totalMinted.current();
+        _mintToken(totalMinted, tokenUri, _msgSender());
+        _totalMinted.increment();
     }
 
     function _mintToken(
@@ -293,7 +214,7 @@ contract Larp is
         address from,
         address to,
         uint256 tokenId
-    ) internal override whenNotPaused {
+    ) internal virtual override(ERC721PresetMinterPauserAutoId, ERC721) {
         super._beforeTokenTransfer(from, to, tokenId);
     }
 
@@ -309,7 +230,7 @@ contract Larp is
     function tokenURI(uint256 tokenId)
         public
         view
-        override(ERC721, ERC721URIStorage)
+        override(ERC721, ERC721URIStorage, IERC721Metadata)
         returns (string memory)
     {
         return super.tokenURI(tokenId);
@@ -330,7 +251,7 @@ contract Larp is
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721, AccessControl)
+        override(ERC721PresetMinterPauserAutoId, IERC165, ERC721)
         returns (bool)
     {
         return
