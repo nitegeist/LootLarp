56d068e6412bf8e49326394003c7b7c3ddd4c9ac
Nitegeist <delroybicknell@gmail.com>
2021-11-29 11:06:05 -0500

    Update private mint tests

--- test/privateMint.js
+++ test/privateMint.js
@@ -10,6 +10,7 @@ describe('Private Mint', function () {
 	let redemptionFactory, redemptionContract;
 	let maxSupply = 508;
 	let payment = utils.parseEther('0.25');
+	const merkleTree = {};
 
 	beforeEach(async function () {
 		redemptionFactory = await hre.ethers.getContractFactory('Redemption');
@@ -20,41 +21,39 @@ describe('Private Mint', function () {
 		redemptionContract = await redemptionFactory.deploy(0, 0, 0);
 		await redemptionContract.deployed();
 		await redemptionContract.batchGrantPreferredMinterRole(addresses);
+		merkleTree.leaves = accounts.map((account) => bufferToHex(utils.solidityKeccak256(['address'], [account.address])));
+		merkleTree.tree = new MerkleTree(merkleTree.leaves, keccak256, { sort: true });
+		merkleTree.root = merkleTree.tree.getHexRoot();
 	});
 
-	// it('Should revert with private mint not active', async function () {
-	// 	await redemptionContract.grantRole(redemptionContract.PREFERRED_MINTER_ROLE(), buyer.address);
-	// 	expect(
-	// 		await redemptionContract.connect(buyer.address).privateMint(2, merkleTree.proof[buyer.address], merkleTree.root)
-	// 	).to.be.revertedWith('Private Mint: Private mint is not active');
-	// });
+	it('Should revert with private mint not active', async function () {
+		await redemptionContract.grantRole(redemptionContract.PREFERRED_MINTER_ROLE(), buyer.address);
+		accounts.push(buyer);
+		merkleTree.leaves = accounts.map((account) => bufferToHex(utils.solidityKeccak256(['address'], [account.address])));
+		merkleTree.tree = new MerkleTree(merkleTree.leaves, keccak256, { sort: true });
+		merkleTree.root = merkleTree.tree.getHexRoot();
+		const leaf = bufferToHex(utils.solidityKeccak256(['address'], [buyer.address]));
+		const proof = merkleTree.tree.getHexProof(leaf);
+		await expect(redemptionContract.connect(buyer).privateMint(2, proof, merkleTree.root)).to.be.revertedWith(
+			'Private Mint: Private mint is not active'
+		);
+	});
 
 	it('Should return true for a valid merkle proof', async function () {
 		accounts.map(async (account) => {
-			const leaves = accounts.map((account) => bufferToHex(utils.solidityKeccak256(['address'], [account.address])));
-			const tree = new MerkleTree(leaves, keccak256, { sort: true });
-			const root = tree.getHexRoot();
-			// MerkleTree.print(tree);
 			const leaf = bufferToHex(utils.solidityKeccak256(['address'], [account.address]));
-			// console.log('Leaf: %s', leaf);
-			const proof = tree.getHexProof(leaf);
-			// console.log('Proof: %s', proof);
-			expect(await redemptionContract.isPreferredMinter(proof, root, account.address)).to.be.true;
+			const proof = merkleTree.tree.getHexProof(leaf);
+			expect(await redemptionContract.isPreferredMinter(proof, merkleTree.root, account.address)).to.be.true;
 		});
 	});
 
 	it('Should return false for a invalid merkle proof', async function () {
-		accounts.map(async (account) => {
-			const leaves = accounts.map((account) => bufferToHex(utils.solidityKeccak256(['address'], [account.address])));
-			const tree = new MerkleTree(leaves, keccak256, { sort: true });
-			const root = tree.getHexRoot();
-			// MerkleTree.print(tree);
-			const leaf = bufferToHex(utils.solidityKeccak256(['address'], [buyer.address]));
-			// console.log('Leaf: %s', leaf);
-			const proof = tree.getHexProof(leaf);
-			// console.log('Proof: %s', proof);
-			expect(await redemptionContract.isPreferredMinter(proof, root, buyer.address)).to.be.false;
-		});
+		// MerkleTree.print(tree);
+		const leaf = bufferToHex(utils.solidityKeccak256(['address'], [buyer.address]));
+		// console.log('Leaf: %s', leaf);
+		const proof = merkleTree.tree.getHexProof(leaf);
+		// console.log('Proof: %s', proof);
+		expect(await redemptionContract.isPreferredMinter(proof, merkleTree.root, buyer.address)).to.be.false;
 	});
 
 	it('Should batch revoke preferred minter role', async function () {
