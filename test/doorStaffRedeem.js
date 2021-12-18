3ed11df36cc5d74a19cabfaf65548007bd3f959e
Carl Farterson <carlfarterson@gmail.com>
2021-12-17 19:17:36 -0800

    add stuff

--- test/doorStaffRedeem.js
+++ test/doorStaffRedeem.js
@@ -17,7 +17,16 @@ describe('Door Staff Redeem', function () {
 		merkleTree.leaves = accounts.map((account) => bufferToHex(utils.solidityKeccak256(['address'], [account.address])));
 		merkleTree.tree = new MerkleTree(merkleTree.leaves, keccak256, { sort: true });
 		merkleTree.root = merkleTree.tree.getHexRoot();
+<<<<<<< Updated upstream
 		redemptionContract = await redemptionFactory.deploy(0, 0, 0, merkleTree.root);
+=======
+
+		const now = new Date(new Date().getTime() + 48 * 60 * 60 * 1000);
+		const start = (now.getTime() / 1000).toFixed(0);
+		const end = (now.setHours(now.getHours() + 72) / 1000).toFixed(0);
+
+		redemptionContract = await redemptionFactory.deploy(start, end, merkleTree.root);
+>>>>>>> Stashed changes
 		await redemptionContract.deployed();
 	});
 
@@ -27,6 +36,7 @@ describe('Door Staff Redeem', function () {
 		).to.be.revertedWith('Door Mint: Door staff mint is not active');
 	});
 
+<<<<<<< Updated upstream
 	it('Should activate door staff redeeem and redeem tokens for buyer', async function () {
 		const now = new Date();
 		const start = (now.getTime() / 1000).toFixed(0);
@@ -36,6 +46,17 @@ describe('Door Staff Redeem', function () {
 		await redemptionContract.grantRole(redemptionContract.MINTER_ROLE(), accounts[0].address);
 		await redemptionContract.connect(buyer).payDoorStaff(2, { value: utils.parseEther('0.5') });
 		await redemptionContract.connect(accounts[0]).doorStaffRedeem(2, buyer.address, { value: utils.parseEther('0.5') });
+=======
+	it('Should activate door staff redeem and redeem tokens for buyer', async function () {
+		await network.provider.request({
+			method: 'evm_increaseTime',
+			params: [3600 * 49],
+		});
+		await redemptionContract.connect(owner).initialize(merkleTree.root);
+		await redemptionContract.grantRole(redemptionContract.MINTER_ROLE(), doorStaff.address);
+		await redemptionContract.connect(buyer).payDoorStaff(2, { value: utils.parseEther('1') });
+		await redemptionContract.connect(doorStaff).doorStaffRedeem(2, buyer.address, { value: utils.parseEther('1') });
+>>>>>>> Stashed changes
 		expect(await redemptionContract.balanceOf(buyer.address)).to.equal(2);
 	});
 
